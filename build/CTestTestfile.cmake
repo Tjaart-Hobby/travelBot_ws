8c8e678b661d5b44248adba1f119f95e2c7713fc
Tjaart-Hobby <tjaart.kot@gmail.com>
2023-04-24 07:51:41 +0800

    try6

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/tjaart/travelBot_ws/src
+# Build directory: /home/tjaart/travelBot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("travel_bot_description")
+subdirs("travel_bot_gazebo")
---
+++ build/travel_bot_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tjaart/travelBot_ws/src/travel_bot_gazebo
+# Build directory: /home/tjaart/travelBot_ws/build/travel_bot_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
